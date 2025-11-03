00595d55d5742b5196c390371b318be010f33712
jonasjchavez-bit <jonasjchavez@gmail.com>
2025-11-03 20:16:43 +0000

    generador_URL_Joseph

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let pronoun = ['the', 'our', 'my', 'your', 'his', 'super'];
+let adj = ['great', 'big','fast', 'smart', 'cool', 'happy'];
+let noun = ['jogger', 'racoon', 'wizard', 'rocket', 'planet', 'ninja'];
+let extensions = ['.com', '.net', '.us', '.io', '.org', '.dev'];
+let excuseElement= document.querySelector("#excuse");
+
+
+pronoun.forEach(pron => {
+  adj.forEach(adj => {
+    noun.forEach(sus => {
+      let nombreDeDominios = pron+adj+sus;
+      extensions.forEach(ext => {
+        if (sus.endsWith (ext.slice(1))){
+          console.log (pron + adj + sus.slice(0, sus.length -ext.slice(1).length)+ext);
+        }
+        else{
+          let nombreDeDominiosConExt=nombreDeDominios+ext;
+          excuseElement.innerHTML+= nombreDeDominiosConExt + "<br>";
+          console.log(nombreDeDominiosConExt);
+        }
+     })
+    })
+  })
+})
+
