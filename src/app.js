5f9e428f001fe80c7dbec18c1ed8e0bb01b9bdfb
jonasjchavez-bit <jonasjchavez@gmail.com>
2025-11-04 19:41:25 +0000

    Lineamientos de Codigo Joseph

--- src/app.js
+++ src/app.js
@@ -7,26 +7,26 @@ import "./assets/img/4geeks.ico";
 
 
 let pronoun = ['the', 'our', 'my', 'your', 'his', 'super'];
-let adj = ['great', 'big','fast', 'smart', 'cool', 'happy'];
+let adj = ['great', 'big', 'fast', 'smart', 'cool', 'happy'];
 let noun = ['jogger', 'racoon', 'wizard', 'rocket', 'planet', 'ninja'];
 let extensions = ['.com', '.net', '.us', '.io', '.org', '.dev'];
-let excuseElement= document.querySelector("#excuse");
+let excuseElement = document.querySelector("#excuse");
 
 
 pronoun.forEach(pron => {
   adj.forEach(adj => {
     noun.forEach(sus => {
-      let nombreDeDominios = pron+adj+sus;
+      let nombreDeDominio = pron + adj + sus;
       extensions.forEach(ext => {
-        if (sus.endsWith (ext.slice(1))){
-          console.log (pron + adj + sus.slice(0, sus.length -ext.slice(1).length)+ext);
+        if (sus.endsWith(ext.slice(1))) {
+          console.log(pron + adj + sus.slice(0, sus.length - ext.slice(1).length) + ext);
         }
-        else{
-          let nombreDeDominiosConExt=nombreDeDominios+ext;
-          excuseElement.innerHTML+= nombreDeDominiosConExt + "<br>";
+        else {
+          let nombreDeDominiosConExt = nombreDeDominio + ext;
+          excuseElement.innerHTML += nombreDeDominiosConExt + "<br>";
           console.log(nombreDeDominiosConExt);
         }
-     })
+      })
     })
   })
 })
